1794c83530058e31e70db7331cc9191700f2f223
yuyuyu <rikki.k2008@gmail.com>
2020-04-26 10:48:10 +0900

    auto commit

--- autocommit.sh
+++ autocommit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-while true
-do
   git add ./autocommit.sh
   git add ./github/source
   git add ./django/source
   git add ./sphinx/source
+  git status
   git commit -m 'auto commit'
   git push sphinx master
   echo "-----------------------"
-  sleep 300
-done
+
+exit 0
+
